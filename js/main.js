46e33923d2ac0acddd879d886d91f0640631935d
godwin opara <godwinopara62@yahoo.com>
2021-10-30 22:22:56 +0400

    added javascript functionality that updates the cart anytime the user increases the quantity of the product

--- js/main.js
+++ js/main.js
@@ -87,3 +87,92 @@ thumbnailImages.forEach((img, index) => {
     productImages[current].style.display = "block";
   });
 });
+
+// increase or decrease the total pieces of the product the user wants
+
+function updateProductQuantity() {
+  const productQuantity = document.querySelector(".quantity");
+  const increaseQuantity = document.querySelector(".plus-btn");
+  const decreaseQuantity = document.querySelector(".minus-btn");
+
+  increaseQuantity.addEventListener("click", () => {
+    productQuantity.innerHTML = `${Number(productQuantity.innerHTML) + 1}`;
+  });
+  decreaseQuantity.addEventListener("click", () => {
+    if (productQuantity.innerHTML !== "1") {
+      productQuantity.innerHTML = `${Number(productQuantity.innerHTML) - 1}`;
+    }
+  });
+}
+
+updateProductQuantity();
+
+function updateCart(totalQuantity, total) {
+  const cartUpdate = `
+
+ <div class="cart__item__details">
+    <div class="item__details">
+      <img
+        class="item__img"
+        src="/images/image-product-1-thumbnail.jpg"
+        alt=""
+      />
+      <div class="item__name">
+        <p>Autumn Limited Edition..</p>
+        <span>$125.00</span>
+        <span>&times;</span>
+        <span>${totalQuantity}</span>
+        <span>$${total}</span>
+      </div>
+      <div onClick="deleteItem()" class="delete-icon">
+        <img src="/images/icon-delete.svg" alt="" />
+      </div>
+    </div>
+</div>
+<div class="checkout-btn">
+  <button>Checkout</button>
+</div>
+  `;
+
+  return cartUpdate;
+}
+
+function addToCart() {
+  const addToCartBtn = document.querySelector(".btn__add__to__cart");
+  addToCartBtn.addEventListener("click", (e) => {
+    const getProductQuantity = document.querySelector(".quantity").innerHTML;
+    const cartContainer = document.querySelector(".cart__item__container");
+    const totalCost = (Number(getProductQuantity) * 125).toString();
+    cartContainer.innerHTML = updateCart(getProductQuantity, totalCost);
+  });
+}
+
+addToCart();
+
+const getProductQuantity = document.querySelector(".quantity").innerHTML;
+function deleteItem() {
+  if (Number(getProductQuantity) > 1) {
+    // let newQuantity = Number(getProductQuantity) - 1;
+    getProductQuantity.innerHTML = Number(getProductQuantity) - 1;
+    console.log(getProductQuantity);
+  }
+}
+
+//  <div class="cart__item__details">
+//                 <div class="item__details">
+//
+//                   <div class="item__name">
+//                     <p>Autumn Limited Edition..</p>
+//                     <span>$125.00</span>
+//                     <span>&times;</span>
+//                     <span>3</span>
+//                     <span>$375.00</span>
+//                   </div>
+//                   <div class="delete-icon">
+//                     <img src="/images/icon-delete.svg" alt="" />
+//                   </div>
+//                 </div>
+//               </div>
+//               <div class="checkout-btn">
+//                 <button>Checkout</button>
+//               </div>
